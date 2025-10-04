4efc48ac8cfab657f82bbc2cf342547a967fc504
Vibesh Vidya <vibesh.vidya@gmail.com>
2025-10-05 02:43:05 +0530

    file updated

--- samplepython.py
+++ samplepython.py
@@ -1,2 +1,10 @@
 print("Welcome to Python programming!")
-print("Welcome to Python programming!")
+#Step 3: Variables & Data Types
+name = "Vibesh"
+print (name)
+age = 20
+print (age)
+height = 5.9
+print (height)
+is_student = False
+print (is_student)
